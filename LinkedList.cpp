fa6077686abfb608ca1a5f8b09feae277ad3b22c
Shreyas <shreyassingh621@gmail.com>
2023-07-23 05:54:44 +0530

    80 percent basics done

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -17,3 +17,104 @@ class Node {
     }
 
 };
+
+class List {
+
+    Node * head, * tail;
+    public:
+    List (){
+        head = tail = 0;
+    }
+    
+    void addtohead(int el);
+    void addtotail(int el);
+    void deletehead();
+    void deletetail();
+    void search(int el);
+    void middle();
+    void display();
+
+};
+
+void List :: addtohead(int el){
+    
+     head = new Node(el,head);
+     if (tail == 0){
+        tail = head;
+     }
+
+}
+
+void List :: addtotail(int el){
+
+     tail->next = new Node (el,0);
+     tail = tail->next;
+
+}
+
+void List :: deletehead(){
+
+    Node * temp = head;
+    head = temp->next;
+    delete temp;
+}
+
+void List :: deletetail(){
+
+    Node * temp = head;
+    while(temp->next != tail){
+        temp = temp->next;
+    }
+    Node* todelete = temp->next;
+    temp->next = NULL;
+    tail = temp;
+    delete todelete;
+    
+    
+}
+
+void List :: search (int el){
+
+    Node * temp = head;
+    int x = 0;
+    while (temp->data != el){
+        temp = temp -> next; x++;
+        if (temp == 0){ cout<<"Element Does Not Exists"<<endl; break; }
+    }
+    if (temp->data == el){
+        cout<<"Element found at index "<<x<<endl;
+    }
+}
+
+
+
+void List :: display(){
+
+    Node * temp = head;
+    while(temp != 0){
+        cout<<temp->data<<" -> ";
+        temp = temp -> next;
+    }
+    cout<<"NULL"<<endl;
+
+}
+
+int main (){
+    List l;
+    l.addtohead(5);
+    l.addtohead(4);
+    l.addtohead(3);
+    l.addtohead(2);
+    l.addtohead(1);
+    l.addtotail(6);
+    l.deletehead();
+    l.deletetail();
+    l.search(2);
+    l.search(5);
+    l.search(3);
+    l.search(100);
+
+    l.display();
+
+    return 0;
+}
